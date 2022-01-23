920de02b0bc602714773b8a67ceeb9ab1169a22f
sinansubasi95 <sinan95@gmail.com>
2022-01-23 18:05:30 +0300

    starting up a nest app

--- scratch/src/main.ts
+++ scratch/src/main.ts
@@ -1,4 +1,5 @@
 import { Controller, Module, Get } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
 @Controller()
 class AppController {
@@ -14,3 +15,11 @@ class AppController {
 class AppModule {
 
 }
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(3000);
+}
+
+bootstrap();
