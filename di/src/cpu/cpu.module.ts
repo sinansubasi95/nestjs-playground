b807ff6b3e6bfc7b464b81993764b1bb7358f36e
sinansubasi95 <sinan95@gmail.com>
2022-01-27 11:25:49 +0300

    setting up di between modules

--- di/src/cpu/cpu.module.ts
+++ di/src/cpu/cpu.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PowerModule } from 'src/power/power.module';
 import { CpuService } from './cpu.service';
 
 @Module({
-  providers: [CpuService]
+  providers: [CpuService, PowerModule]
 })
-export class CpuModule {}
+export class CpuModule { }
