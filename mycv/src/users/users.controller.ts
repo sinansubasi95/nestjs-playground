d42972aad248beea607aff877aae1e27b683899d
sinansubasi95 <sinan95@gmail.com>
2022-01-30 23:38:28 +0300

    wrapping the interceptor in a decorator

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
-import { SerializeInterceptor } from 'src/interceptors/searialize.interceptor';
+import { Serialize } from 'src/interceptors/searialize.interceptor';
 import { createUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
@@ -14,7 +14,8 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
-    @UseInterceptors(new SerializeInterceptor(UserDto))
+    // @UseInterceptors(new SerializeInterceptor(UserDto))
+    @Serialize(UserDto)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         console.log('handler is running');
