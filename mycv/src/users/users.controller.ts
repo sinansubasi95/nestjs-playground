ffc4e8ae9ac892cc93fbda0a8acefefbff790c8b
sinansubasi95 <sinan95@gmail.com>
2022-01-31 20:45:26 +0300

    handling user sign in

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -16,6 +16,11 @@ export class UsersController {
         return this.authService.signup(body.email, body.password)
     }
 
+    @Post('/signin')
+    signin(@Body() body: createUserDto) {
+        return this.authService.signin(body.email, body.password);
+    }
+
     // @UseInterceptors(new SerializeInterceptor(UserDto))
     // @Serialize(UserDto)
     @Get('/:id')
