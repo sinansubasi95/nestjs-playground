f75f5e4340abec207144d6f3be4196d5432d4ba8
sinansubasi95 <sinan95@gmail.com>
2022-02-03 20:04:20 +0300

    testing the signin flow

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -5,10 +5,11 @@ import { UsersService } from './users.service';
 
 describe('AuthService', () => {
     let service: AuthService;
+    let fakeUsersService: Partial<UsersService>;
 
     beforeEach(async () => {
         // Create a fake copy of the users service
-        const fakeUsersService: Partial<UsersService> = {
+        fakeUsersService = {
             find: () => Promise.resolve([]),
             create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User)
         };
@@ -31,11 +32,29 @@ describe('AuthService', () => {
     });
 
     it('creates a new user with a salted and hashed password', async () => {
-        const user = await service.signup('sada@weqe.ewq', 'asdf');
+        const user = await service.signup('asdf@asdf.com', 'asdf');
 
         expect(user.password).not.toEqual('asdf');
         const [salt, hash] = user.password.split('');
         expect(salt).toBeDefined();
         expect(hash).toBeDefined();
+    });
+
+    it('throws an error if user signs up with email that is in use', async (done) => {
+        fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+
+        try {
+            await service.signup('asdf@asdf.com', 'asdf');
+        } catch (err) {
+            done();
+        }
+    });
+
+    it('throws if signin is called with an unused email', async (done) => {
+        try {
+            await service.signin('wqewqe@weqewq.ewq', 'wqewq');
+        } catch (err) {
+            done();
+        }
     });
 });
