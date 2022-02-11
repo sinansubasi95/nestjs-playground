1646556ba7793d3cd295646f9608960b04080eb0
sinansubasi95 <sinan95@gmail.com>
2022-02-11 17:11:18 +0300

    receiving report creation requests

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
-    @Post()
-    createReport(@Body() body: CreateReportDto) {
+    constructor(private reportsService: ReportsService) { }
 
+    @Post()
+    @UseGuards(AuthGuard)
+    createReport(@Body() body: CreateReportDto) {
+        return this.reportsService.create(body);
     }
 }
